6dc8fdefbd958c77d9103b680bc4aca0c2a69d57
Paco <pacocanovaca97@gmail.com>
2026-03-26 18:11:34 +0100

    Importación librería Lombok y creación de la estructura de directorios, ficheros y clases Java inicial (sujeta a cambios)

---
+++ Login/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Cuando se ejecuta el programa, debe importarse la lista de Admins y la lista de Workers de los archivos .obj
+
+
+
+        // Cuando acaba la ejecución del programa, se vuelve a importar la lista de Admin y la lista de Workers a los archivos .obj
+
+    }
+}
