67d2f0e49d98e1189dea9d163a63e9b22553d37a
Paco <pacocanovaca97@gmail.com>
2026-04-06 18:51:18 +0200

    Conexión de LoginController con las pestañas de Admin y Worker (en función de los datos introducidos) y gestión de errores (datos sin introducir o incorrectos)

---
+++ SistemaLogin/src/main/java/org/example/sistemalogin/controller/AdminController.java
@@ -0,0 +1,4 @@
+package org.example.sistemalogin.controller;
+
+public class AdminController {
+}
--- SistemaLogin/src/main/java/org/example/sistemalogin/controller/LoginController.java
+++ SistemaLogin/src/main/java/org/example/sistemalogin/controller/LoginController.java
@@ -1,17 +1,18 @@
 package org.example.sistemalogin.controller;
-import com.sun.security.jgss.GSSUtil;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 import org.example.sistemalogin.model.Admin;
-import org.example.sistemalogin.model.User;
 import org.example.sistemalogin.model.Worker;
 import org.example.sistemalogin.HelloApplication;
 
+import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
@@ -42,19 +43,29 @@ public class LoginController implements Initializable {
 
     public void instances() {
         admins = FXCollections.observableArrayList(new Admin("admin", "admin", "123456A", "admin@admin.com", "admin"));
-        workers = FXCollections.observableArrayList(); // Ambas inicializaciones temporales hasta importar usuarios de los archivos .obj
+        workers = FXCollections.observableArrayList(new Worker("worker", "worker", "234566B", "worker@worker.com", "worker")); // Ambas inicializaciones temporales hasta importar usuarios de los archivos .obj
     }
 
     public void actions() {
          send.setOnAction(event -> {
             String username = user.getText();
             String pass = password.getText();
-            if (checkAdmin(username, pass)) {
-                System.out.println("Eres un admin");
+            if (username.isEmpty() || pass.isEmpty()) {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Error");
+                alert.setContentText("Debes introducir un usuario y una contraseña. Ambos campos deben tener un valor.");
+                alert.show();
+            } else if (checkAdmin(username, pass)) {
+                generateStage("admin-view.fxml", "Administración de usuarios");
             } else if (checkWorker(username, pass)) {
-                System.out.println("Eres un trabajador");
+                generateStage("worker-view.fxml", "Área de trabajador");
             } else {
-                System.out.println("El usuario o la contraseña no son correctos");
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Error");
+                alert.setContentText("El usuario o la contraseña no son correctos. Vuelve a introducir los datos.");
+                alert.show();
+                user.setText("");
+                password.setText("");
             }
         });
     }
@@ -77,6 +88,23 @@ public class LoginController implements Initializable {
             }
         }
         return false;
+    }
+
+    public void generateStage(String resource, String title) {
+        Stage stage = new Stage();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(resource));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle(title);
+            stage.setScene(scene);
+            stage.show();
+            ((Stage)send.getScene().getWindow()).close();
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Error");
+            alert.setContentText("No se ha encontrado la pantalla que intenta cargar.");
+            alert.show();
+        }
     }
 
 }
